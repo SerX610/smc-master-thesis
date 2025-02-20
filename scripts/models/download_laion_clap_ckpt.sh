b305e0fd7cc292eb3818e47d1290b97b5e2e04f4
Sergio Cárdenas <serxpiano@gmail.com>
2025-02-20 21:34:03 +0100

    feat: add script to download laion-clap checkpoint

---
+++ scripts/models/download_laion_clap_ckpt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define the model checkpoint to download and its URL
+MODEL_CKPT="music_audioset_epoch_15_esc_90.14.pt"
+URL="https://huggingface.co/lukewys/laion_clap/resolve/main/$MODEL_CKPT"
+
+# Define the destination directory and create it if it doesn't exist
+DEST_DIR="../../models/laion-clap"
+mkdir -p "$DEST_DIR"
+
+# Download the file using wget
+wget -O "$DEST_DIR/$MODEL_CKPT" "$URL"
+
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+    echo "Download completed successfully."
+else
+    echo "Download failed. Please check the URL and try again."
+    exit 1
+fi
